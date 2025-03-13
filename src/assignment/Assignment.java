77f7eafaf8b2a9326670072d7f7a0eb2bdc5988e
HappyHat952 <senrenjp@gmail.com>
2025-03-13 11:00:41 -0400

    assignments are seperated and drawn

--- src/assignment/Assignment.java
+++ src/assignment/Assignment.java
@@ -12,12 +12,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Assignment extends BasicGameState {
     private int id;
     private StateBasedGame sbg;
 
     private ArrayList<String> assignment;
+    private ArrayList<MultipleChoice> mcqs;
 
     public Assignment(int id) {
         this.id = id;
@@ -31,6 +33,7 @@ public class Assignment extends BasicGameState {
         // This code happens when you enter a game state for the *first time.*
         gc.setShowFPS(true);
         this.sbg= sbg;
+        mcqs = new ArrayList<>();
         setAssignment("test1");
     }
 
@@ -45,11 +48,15 @@ public class Assignment extends BasicGameState {
         g.drawRect(50,0,Main.getScreenWidth() - 100, Main.getScreenHeight() );
         g.setColor(Color.black);
         g.drawString("Assignment:", Main.getScreenWidth() * .5f, Main.getScreenHeight() * .1f);
-        float y = Main.getScreenHeight()*.1f;
-        for (String s: assignment)
+//        float y = Main.getScreenHeight()*.1f;
+//        for (String s: assignment)
+//        {
+//            y+= 20;
+//            g.drawString(s,Main.getScreenWidth()*.2f, y);
+//        }
+        if (mcqs!= null && !mcqs.isEmpty())
         {
-            y+= 20;
-            g.drawString(s,Main.getScreenWidth()*.2f, y);
+            mcqs.getFirst().draw(g);
         }
 
     }
@@ -78,13 +85,27 @@ public class Assignment extends BasicGameState {
             File f = new File("res/assignment/"+id+".txt");
             Scanner s = new Scanner(f);
             String str = s.nextLine();
+            ArrayList<String> q = new ArrayList<>();
 
             while (!str.equals("_"))
             {
+                if (Pattern.matches("[\\D.]", str.substring(0,0)))
+                {
+                    if (q.isEmpty())
+                    {
+                        q = new ArrayList<>();
+                    }
+                    else {
+                        mcqs.add (new MultipleChoice(q));
+                        q = new ArrayList<>();
+                    }
+                }
+                q.add(str);
                 System.out.print(str);
                 assignment.add(str);
                 str = s.nextLine();
             }
+            MultipleChoice mcq = new MultipleChoice(assignment);
         } catch (FileNotFoundException e) {
             System.out.println("fine wasn't found");
         }
---
+++ src/assignment/MultipleChoice.java
@@ -0,0 +1,59 @@
+package assignment;
+
+import core.Main;
+import org.newdawn.slick.Graphics;
+
+import java.util.ArrayList;
+
+public class MultipleChoice {
+    //will contain all options + the question.
+    ArrayList<String> mcq;
+    int number;
+
+    String header;
+    ArrayList<String> options;
+    char correct;
+
+    public MultipleChoice(ArrayList<String> mcq)
+    {
+        this.mcq = mcq;
+        setInfo();
+    }
+
+    public void draw(Graphics g)
+    {
+        float y = Main.getScreenHeight()*.1f+20;
+        float x = Main.getScreenWidth()*.5f;
+        g.drawString(number +". "+header, x, y);
+        for (String s: options)
+        {
+            y+=20;
+            if (s.charAt(0)== correct)
+            {
+                g.drawString(s,x,y);
+            }
+
+        }
+    }
+
+    public void setInfo()
+    {
+        try{
+            //number = Integer.parseInt(mcq.getFirst().replaceAll("[//D]", ""));
+            String s = mcq.getFirst().substring(0,3).replaceAll("[\\D.]", "");
+            number = Integer.parseInt(s);
+            if (number/10 == 0)
+            {
+                header = mcq.getFirst().substring(3,mcq.getFirst().length());
+            }
+            options = mcq;
+            correct = mcq.getLast().charAt(0);
+            options.remove(0);
+            options.remove(1);
+        } catch (NumberFormatException e) {
+            System.out.println(e.getCause());
+            e.printStackTrace();
+        }
+
+    }
+}
